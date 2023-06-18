43587d33bf548dc6f3d31f6f49eaa0f3610a2656
Nahid1911 <nahid.raihan@gmail.com>
2023-06-19 00:08:12 +0600

    complete webpage

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
+
 import './index.css';
 import App from './App';
 import { store } from './store';
 import { Provider } from 'react-redux';
 
+const container = document.getElementById('root');
+const root = createRoot(container);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-    <App />
+      <App />
     </Provider>
   </React.StrictMode>
 );
